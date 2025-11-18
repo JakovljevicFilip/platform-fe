d69b785c78115399ac8bba996c23287c9b51d796
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-11-18 10:24:14 +0100

    Adds config support

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,9 @@
 </template>
 
 <script setup lang="ts">
-//
+  import PlatformConfig from 'src/config/platform-config'
+  import MicroserviceConfig from 'src/config/microservice-config'
+
+  console.log('Platform example:', PlatformConfig.example)
+  console.log('Microservice example:', MicroserviceConfig.example)
 </script>

5cd5112e5ef6e3495cdb24e8aebdc32584ef7f79
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-11-18 11:08:36 +0100

    Adds README

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,4 @@
   <router-view />
 </template>
 
-<script setup lang="ts">
-  import { write } from './application/Platform/Log/Application/log-service'
-
-  write('Hello World!')
-</script>
+<script setup lang="ts"></script>
